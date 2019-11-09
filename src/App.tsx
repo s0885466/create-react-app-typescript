8ab4d85ecb5946eab161caaa0f319d0074d052c7
Aleksander <0885466@gmail.com>
2019-11-09 21:45:39 +0300

    [3] Create checkBox
    src/components/CheckBox/.sass-cache/0600db235fea65dd1736f1c98110c371b945bb63/CheckBox.scssc

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import Module1 from './modules/Module1';
+import Module2 from './modules/Module2';
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <header className="App-header">
         <Module1/>
+        <Module2/>
       </header>
     </div>);
 };
